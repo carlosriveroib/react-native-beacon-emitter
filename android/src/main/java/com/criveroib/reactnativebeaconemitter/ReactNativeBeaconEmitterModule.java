9c41bba9d5e85e73f65e9c7bad57abfacbafaa19
Carlos Rivero <criveroib@gmail.com>
2023-07-20 12:36:44 +0200

    fix: fixed beacon library version

--- android/src/main/java/com/criveroib/reactnativebeaconemitter/ReactNativeBeaconEmitterModule.java
+++ android/src/main/java/com/criveroib/reactnativebeaconemitter/ReactNativeBeaconEmitterModule.java
@@ -1,6 +1,7 @@
 package com.criveroib.reactnativebeaconemitter;
 
 import androidx.annotation.NonNull;
+import android.bluetooth.le.AdvertiseSettings;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -8,25 +9,57 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
+import java.util.Arrays;
+
 @ReactModule(name = ReactNativeBeaconEmitterModule.NAME)
 public class ReactNativeBeaconEmitterModule extends ReactContextBaseJavaModule {
-  public static final String NAME = "ReactNativeBeaconEmitter";
+    public static final String NAME = "ReactNativeBeaconEmitter";
+    private final ReactApplicationContext context;
+    private static BeaconTransmitter beaconTransmitter = null;
 
-  public ReactNativeBeaconEmitterModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+    public ReactNativeBeaconEmitterModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.context = reactContext;
+    }
 
-  @Override
-  @NonNull
-  public String getName() {
-    return NAME;
-  }
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
 
+    @ReactMethod
+    public void start(String uuid, int major, int minor) {
+        Beacon beacon = new Beacon.Builder()
+            .setId1(uuid)
+            .setId2(String.valueOf(major))
+            .setId3(String.valueOf(minor))
+            .setManufacturer(0x0118)
+            .setTxPower(-59)
+            .setDataFields(Arrays.asList(new Long[] {0l}))
+            .build();
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
-  }
+        BeaconParser beaconParser = new BeaconParser()
+            .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+
+        this.beaconTransmitter = new BeaconTransmitter(context, beaconParser);
+        this.beaconTransmitter.setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY);
+        this.beaconTransmitter.setAdvertiseTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH);
+
+        this.beaconTransmitter.startAdvertising(beacon);
+    }
+
+    @ReactMethod
+    public void stop() {
+        if (this.beaconTransmitter != null) {
+            try {
+                this.beaconTransmitter.stopAdvertising();
+            } catch (Exception ex) {
+            }
+        }
+    }
 }
